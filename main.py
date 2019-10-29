267518db9f866eff6617dcaba45d642e613b5a06
Oliver Garcia <olivergarciarodriguez7@gmail.com>
2019-10-29 19:08:04 -0400

    Made it prettier

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 def min_max_difference(the_array):
 
-    the_highest_value = the_array[0]
-    the_smallest_number = the_array[0]
+    the_highest_value = False
+    the_smallest_number = False
     for i in the_array:
-        if the_highest_value < i:
-            the_highest_value = i
 
-        if the_smallest_number > i:
+        if the_highest_value == False or the_smallest_number == False:
+            the_highest_value = i
             the_smallest_number = i
+        else:
+            if the_highest_value < i:
+                the_highest_value = i
+
+            if the_smallest_number > i:
+                the_smallest_number = i
 
     return the_highest_value - the_smallest_number
 
